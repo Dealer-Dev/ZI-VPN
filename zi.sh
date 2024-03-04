0e6f03594dc6fedfb716af80d9abe785bf912875
Dealer Services <159734968+Dealer-Dev@users.noreply.github.com>
2024-03-04 01:45:56 -0400

    Update zi.sh

--- zi.sh
+++ zi.sh
@@ -2,16 +2,16 @@
 # Zivpn UDP Module installer
 # Creator Zahid Islam
 
-echo -e "Updating server"
+echo -e "🐰 Actualizando recursos 🐰"
 sudo apt-get update && apt-get upgrade -y
 systemctl stop zivpn.service 1> /dev/null 2> /dev/null
-echo -e "Downloading UDP Service"
+echo -e "Descargando servicios UDP"
 wget https://github.com/zahidbd2/udp-zivpn/releases/download/udp-zivpn_1.4.9/udp-zivpn-linux-amd64 -O /usr/local/bin/zivpn 1> /dev/null 2> /dev/null
 chmod +x /usr/local/bin/zivpn
 mkdir /etc/zivpn 1> /dev/null 2> /dev/null
 wget https://raw.githubusercontent.com/zahidbd2/udp-zivpn/main/config.json -O /etc/zivpn/config.json 1> /dev/null 2> /dev/null
 
-echo "Generating cert files:"
+echo "Generando certificado:"
 openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=California/L=Los Angeles/O=Example Corp/OU=IT Department/CN=zivpn" -keyout "/etc/zivpn/zivpn.key" -out "/etc/zivpn/zivpn.crt"
 sysctl -w net.core.rmem_max=16777216 1> /dev/null 2> /dev/null
 sysctl -w net.core.wmem_max=16777216 1> /dev/null 2> /dev/null
@@ -36,8 +36,8 @@ NoNewPrivileges=true
 WantedBy=multi-user.target
 EOF
 
-echo -e "ZIVPN UDP Passwords"
-read -p "Enter passwords separated by commas, example: pass1,pass2 (Press enter for Default 'zi'): " input_config
+echo -e "Defina su contraseña a continuación:"
+read -p "Escriba su nueva contraseña: (al dar enter la contraseña por default será 'dealer'): " input_config
 
 if [ -n "$input_config" ]; then
     IFS=',' read -r -a config <<< "$input_config"
@@ -59,4 +59,4 @@ iptables -t nat -A PREROUTING -i $(ip -4 route ls|grep default|grep -Po '(?<=dev
 ufw allow 6000:19999/udp
 ufw allow 5667/udp
 rm zi.* 1> /dev/null 2> /dev/null
-echo -e "ZIVPN UDP Installed"
+echo -e "PROTOCOLO ZIVPN INSTALADO CON ÉXITO 🐰"
